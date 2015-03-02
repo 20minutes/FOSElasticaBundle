e3e2b36bfb6f5241e3ca93950df132594f969fcd
David Allix <webda2l@gmail.com>
2015-03-02 11:04:44 +0100

    PHPCR support

--- DependencyInjection/FOSElasticaExtension.php
+++ DependencyInjection/FOSElasticaExtension.php
@@ -523,7 +523,6 @@ class FOSElasticaExtension extends Extension
     {
         switch ($typeConfig['driver']) {
             case 'orm':
-            case 'phpcr':
                 $eventsClass = '\Doctrine\ORM\Events';
                 break;
             case 'phpcr':
